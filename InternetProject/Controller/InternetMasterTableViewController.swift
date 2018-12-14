f9756b2d107f81e0ba407b53ae122f9bac3b714e
rphi8796 <jlkfop@gmail.com>
2018-12-14 13:26:29 -0700

    i dont know what i did

---
+++ InternetProject/Controller/InternetMasterTableViewController.swift
@@ -0,0 +1,123 @@
+//
+//  InternetMasterTableViewController.swift
+//  InternetProject
+//
+//  Created by Phillips, Ryan on 12/14/18.
+//  Copyright © 2018 Phillips, Ryan. All rights reserved.
+//
+
+import UIKit
+
+class InternetMasterTableViewController: UITableViewController
+{
+    
+    private weak var internetDetail : InternetDetailViewController? = nil
+    private lazy var internetTopics : [String] = [String]()
+    private lazy var addresses : [String] = [String]()
+    
+    private func setupDetailContents() -> Void
+    {
+        internetTopics = [
+            "Internet Definitions 🇮🇱",
+            "Standard Search Engine ❌🇯🇴",
+            "AP CSP 🇺🇸🇮🇱😃",
+            "CTEC 🤠",
+            "Social media 🤑"
+    ]
+        addresses = [
+            "https://apcentral.collegeboard.org/courses/ap-computer-science-principles/course",
+            "http://www.canyonsdistrict.org/",
+            "https://ctec.canyonsdistrict.org/",
+            "https://docs.swift.org/swift-book/LanguageGuide/TheBasics.html",
+            "https://twitter.com/?lang=en"
+    ]
+
+        if let splitView = splitViewController
+        {
+            let currentControllers = splitView.viewControllers
+            internetDetail = currentControllers[0] as? InternetDetailViewController
+        }
+    }
+    
+    //This method only happens once with the split view
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        setupDetailContents()
+        self.clearsSelectionOnViewWillAppear = false
+        
+        if let split = splitViewController
+        {
+            let controllers = split.viewControllers
+            internetDetail = (controllers[controllers.count-1] as! UINavigationController).topViewController as? InternetDetailViewController
+        }
+    }
+
+    // MARK: - Table view data source
+
+    override func numberOfSections(in tableView: UITableView) -> Int {
+        // #warning Incomplete implementation, return the number of sections
+        return 0
+    }
+
+    override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
+        // #warning Incomplete implementation, return the number of rows
+        return 0
+    }
+
+    /*
+    override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
+        let cell = tableView.dequeueReusableCell(withIdentifier: "reuseIdentifier", for: indexPath)
+
+        // Configure the cell...
+
+        return cell
+    }
+    */
+
+    /*
+    // Override to support conditional editing of the table view.
+    override func tableView(_ tableView: UITableView, canEditRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the specified item to be editable.
+        return true
+    }
+    */
+
+    /*
+    // Override to support editing the table view.
+    override func tableView(_ tableView: UITableView, commit editingStyle: UITableViewCellEditingStyle, forRowAt indexPath: IndexPath) {
+        if editingStyle == .delete {
+            // Delete the row from the data source
+            tableView.deleteRows(at: [indexPath], with: .fade)
+        } else if editingStyle == .insert {
+            // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
+        }    
+    }
+    */
+
+    /*
+    // Override to support rearranging the table view.
+    override func tableView(_ tableView: UITableView, moveRowAt fromIndexPath: IndexPath, to: IndexPath) {
+
+    }
+    */
+
+    /*
+    // Override to support conditional rearranging of the table view.
+    override func tableView(_ tableView: UITableView, canMoveRowAt indexPath: IndexPath) -> Bool {
+        // Return false if you do not want the item to be re-orderable.
+        return true
+    }
+    */
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
